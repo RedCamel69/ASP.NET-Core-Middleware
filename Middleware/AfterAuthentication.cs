a354df9fbd9148f768c2e9363826bc3a174186bf
Richard Gilbert <RichardJGilbert@hotmail.com>
2023-03-06 18:46:38 +0000

    Add files via upload

---
+++ Middleware/AfterAuthentication.cs
@@ -0,0 +1,30 @@
+﻿namespace ASP.Net_Core_Middleware.Middleware
+{
+    public class AfterAuthentication
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<AfterAuthentication> _logger;
+
+        public AfterAuthentication(RequestDelegate next, ILogger<AfterAuthentication> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            _logger.LogInformation("ILogger LogInformation from AfterAuthentication.");
+
+            if (context.User.Identity.IsAuthenticated)
+            {
+                _logger.LogInformation("User Authenticated.");
+            }
+            else
+            {
+                _logger.LogInformation("User not authenticated");
+            }
+
+            await _next(context);
+        }
+    }
+}
---
+++ Middleware/ExampleMiddleware.cs
@@ -0,0 +1,26 @@
+﻿namespace ASP.Net_Core_Middleware.Middleware
+{
+    public class ExampleMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExampleMiddleware> _logger;
+
+        public ExampleMiddleware(RequestDelegate next, ILogger<ExampleMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {          
+            _logger.LogTrace("ILogger LogTrace from ExampleMiddleware.");
+            _logger.LogDebug("ILogger LogDebug from ExampleMiddleware.");
+            _logger.LogInformation("ILogger LogInformation from ExampleMiddleware.");
+            _logger.LogWarning("ILogger LogWarning from ExampleMiddleware.");
+            _logger.LogError("ILogger LogError from ExampleMiddleware.");
+            _logger.LogCritical("ILogger LogCritical from ExampleMiddleware.");
+
+            await _next(context);
+        }
+    }
+}
